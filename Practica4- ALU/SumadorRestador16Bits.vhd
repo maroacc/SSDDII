efb07e291f68b17be4cd8a076320c7f4d80338b0
maroacc <45521548+maroacc@users.noreply.github.com>
2019-03-04 08:33:20 +0100

    Sumador Restador Generico

--- Practica4- ALU/SumadorRestador16Bits.vhd
+++ Practica4- ALU/SumadorRestador16Bits.vhd
@@ -1,25 +1,26 @@
--- Sumador para números de 4 bits.
--- Se usa una descripci ón en alto nivel usando el
--- operador suma con tipos unsigned.
+-- Sumador para números de N bits.
+-- Se usa una descripcion en alto nivel usando el
+-- operador suma con tipos signed.
 
 library ieee;
 use ieee. std_logic_1164 .all;
 use ieee. numeric_std .all;
 
 entity Sumador4BitsAN is
+ generic (g_data_w : integer := 16);
  port (
- a, b : in std_logic_vector (15 downto 0); -- Entradas
- s : out std_logic_vector (15 downto 0); -- Salida
+ a, b : in std_logic_vector (g_data_w - 1 downto 0); -- Entradas
+ s : out std_logic_vector (g_data_w - 1 downto 0); -- Salida
  co : out std_logic ); -- Acarreo de salida
 
  end Sumador4BitsAN ;
 
  architecture behavioral of Sumador4BitsAN is
- signal s_int : signed (16 downto 0);
+ signal s_int : signed (g_data_w downto 0);
  begin -- behavioral
 
  s_int <= signed ('0'& a)+ signed ('0'&b);
- s <= std_logic_vector (s_int (15 downto 0));
- co <= s_int (16);
+ s <= std_logic_vector (s_int (g_data_w - 1 downto 0));
+ co <= s_int (g_data_w);
 
  end behavioral ;
